aa589d357806421f4c95b405add8a5934da24a9e
Richard Sahala Hartanto <richard.sh093@gmail.com>
2018-02-08 21:18:35 +0900

    Win_VR UI Input

---
+++ Assets/VR_UI_Item.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR_UI_Item : MonoBehaviour {
+
+	private BoxCollider boxCollider;
+    private RectTransform rectTransform;
+
+    private void OnEnable()
+    {
+        ValidateCollider();
+    }
+
+    private void OnValidate()
+    {
+        ValidateCollider();
+    }
+
+    private void ValidateCollider()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        boxCollider = GetComponent<BoxCollider>();
+        if (boxCollider == null)
+        {
+            boxCollider = gameObject.AddComponent<BoxCollider>();
+        }
+
+        boxCollider.size = rectTransform.sizeDelta;
+    }
+}
